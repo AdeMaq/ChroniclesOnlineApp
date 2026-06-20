74de09ef14c564846825ff8480b6d4a196dcc4bd
AdeMaq <160094094+AdeMaq@users.noreply.github.com>
2026-06-20 23:41:40 +0500

    Applied converts to show status in livestatuspage

---
+++ ChronicleOnline/Converters/StatusToColorConverter.cs
@@ -0,0 +1,32 @@
+﻿using ChronicleOnline.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ChronicleOnline.Converters
+{
+    public class StatusToColorConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is StatusType status)
+            {
+                return status switch
+                {
+                    StatusType.Exception => Colors.Red,
+                    StatusType.NotClockedIn => Colors.Gray,
+                    StatusType.ClockedIn => Colors.Green,
+                    _ => Colors.Yellow
+                };
+            }
+
+            return Colors.Pink;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ChronicleOnline/Converters/StatusToLabelConverter.cs
@@ -0,0 +1,31 @@
+﻿using ChronicleOnline.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ChronicleOnline.Converters
+{
+    public class StatusToLabelConverter:IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is StatusType status)
+            {
+                return status switch
+                {
+                    StatusType.Exception => "Exception",
+                    StatusType.NotClockedIn => "Not Clocked In",
+                    StatusType.ClockedIn => "Clocked In",
+                    _ => "Unknown"
+                };
+            }
+            return string.Empty;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
