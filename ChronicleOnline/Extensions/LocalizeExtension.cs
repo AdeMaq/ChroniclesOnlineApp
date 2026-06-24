a098cd7c1cb95bdff2e412c5ab690963ccf561aa
AdeMaq <160094094+AdeMaq@users.noreply.github.com>
2026-06-24 19:28:47 +0500

    Localization applied for french and urdu

---
+++ ChronicleOnline/Extensions/LocalizeExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChronicleOnline.Services;
+
+namespace ChronicleOnline.Extensions
+{
+    [ContentProperty(nameof(Key))]
+    public class LocalizeExtension: BindableObject, IMarkupExtension<BindingBase>
+    {
+        public string? Key { get; set; }
+        public BindingBase ProvideValue(IServiceProvider serviceProvider)
+        {
+
+            var binding = new Binding
+            {
+                Mode = BindingMode.OneWay,
+                Path = $"[{Key}]",
+                Source = LocalizationManager.Instance
+            };
+            return binding;
+        }
+
+
+        object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider) => ProvideValue(serviceProvider);
+    }
+}
